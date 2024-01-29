cc68a72ff3ae2763795984c8e604cc35848b9923
Gustavo <140539608+Luxipe@users.noreply.github.com>
2024-01-29 14:09:05 -0300

    Add files via upload

---
+++ arquivo 3.js
@@ -0,0 +1,9 @@
+// Declarando variáveis
+let nome = "João";
+let idade = 25;
+
+// Criando mensagem usando operadores de concatenação
+let mensagem = "Nome: " + nome + ", Idade: " + idade;
+
+// Exibindo a mensagem
+console.log(mensagem);
